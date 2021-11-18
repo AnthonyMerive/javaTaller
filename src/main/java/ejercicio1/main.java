1a73a1e5deeb3ef0381b60bd1b8595e4973060a8
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-11-18 16:56:03 -0500

    primer commit

---
+++ src/main/java/ejercicio1/main.java
@@ -0,0 +1,19 @@
+package ejercicio1;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int numA = 3;
+        int numB = 5;
+
+        if (numA > numB) {
+            System.out.println("numA es mayor");
+        } else if (numA < numB) {
+            System.out.println("numB es mayor");
+        } else {
+            System.out.println("numA y numB son iguales");
+        }
+    }
+
+}
---
+++ src/main/java/ejercicio2/main.java
@@ -0,0 +1,31 @@
+package ejercicio2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Introduzca un numero: ");
+        int numA = cargarNum();
+        System.out.println("Introduzca otro numero: ");
+        int numB = cargarNum();
+
+        if (numA > numB) {
+            System.out.println("\n" + numA + " es mayor");
+        } else if (numA < numB) {
+            System.out.println("\n" + numB + " es mayor");
+        } else {
+            System.out.println("\n" + numA + " y " + numB + " son iguales");
+        }
+    }
+
+    static int cargarNum() throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(reader);
+        String num = buffer.readLine();
+        return Integer.parseInt(num);
+    }
+
+}
