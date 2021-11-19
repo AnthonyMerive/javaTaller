e2ba0304053be14c3b16686398be0d3fb9fe3ed9
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-11-19 14:43:52 -0500

    finalizando ejercicio 12, queda una duda

---
+++ src/main/java/ejercicio12/main.java
@@ -0,0 +1,28 @@
+package ejercicio12;
+
+import java.io.*;
+
+public class main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Ingrese una palabra: ");
+        String palabraUno = cargarString().toLowerCase();
+        System.out.println("Ingrese otra palabra: ");
+        String palabraDos = cargarString().toLowerCase();
+
+        if (palabraUno.contains(" ") || palabraDos.contains(" ")) {
+            System.out.println("Introdujo una o mas frases, no una palabras");
+        } else if (palabraUno.equals(palabraDos)) {
+            System.out.println("Las palabras son iguales");
+        } else {
+            System.out.println("Las palabras no son iguales");
+        }
+    }
+
+    static String cargarString() throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(reader);
+        return buffer.readLine();
+    }
+}
