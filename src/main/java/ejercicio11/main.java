61d58575dbcaea16928c507ac9a7782c3bc2dd8a
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-11-19 13:08:05 -0500

    finalizado ejercicio 11

---
+++ src/main/java/ejercicio11/main.java
@@ -0,0 +1,29 @@
+package ejercicio11;
+
+import java.io.*;
+
+public class main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Ingrese una frase: ");
+        String frase = cargarString().replace(" ", "").toLowerCase();
+        int contador = 0;
+
+        char[] arrayFrase = frase.toCharArray();
+
+        for (char letra : arrayFrase) {
+            if (Character.toString(letra).matches("[aeiou]"))
+                contador++;
+        }
+
+        System.out.println("En la frase hay " + contador + " vocales y " + frase.length() + " letras en total");
+
+    }
+
+    static String cargarString() throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(reader);
+        return buffer.readLine();
+    }
+}
