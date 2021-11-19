94f8e7a1ba1ecd1ffaddf3d826bde22cab7d84c5
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-11-19 12:16:49 -0500

    finalizado el ejercicio 9

---
+++ src/main/java/ejercicio9/main.java
@@ -0,0 +1,13 @@
+package ejercicio9;
+
+public class main {
+    public static void main(String[] args) {
+
+        String frase = "La sonrisa sera la mejor arma contra la tristeza";
+        String newFrase = frase.replace("a","e")+" soy Enthony jejeje";
+
+        System.out.println(newFrase);
+
+
+    }
+}
